44e9c0c40c490bd5de1ecc9013819c7f055f1234
beepolyarka123 <112629525+beepolyarka123@users.noreply.github.com>
2022-09-01 22:05:31 +0300

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,15 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World");
+    Console.WriteLine("Welcome to your new app");
+  }
+}
